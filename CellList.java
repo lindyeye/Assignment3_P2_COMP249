727b18b61392d6f6da3c1eb1160a2c06f0917bdc
Linden <wheelerlinden@gmail.com>
2022-12-02 10:24:21 -0500

    started CellList methods

--- CellList.java
+++ CellList.java
@@ -40,4 +40,49 @@ public class CellList implements Cloneable{
         }
         
     }
+    private CellNode head;
+    private static int size = 0;
+
+    public CellList()
+    {
+        head = null;
+    }
+    //copy constructor
+    public CellList(CellList lt)
+    {
+        if(lt.head == null)
+            head = null;
+        else
+        {
+            head = null;
+            CellNode t1,t2,t3;
+
+            t1 = lt.head;
+            t2 = t3 = null;
+            while(t1 != null)
+            {
+                if (head == null)
+                {
+                    t2 = new CellNode(t1);
+                    head = t2;
+                }
+                else
+                {
+                    t3 = new CellNode(t1);
+                    t2.next = t3;
+                    t2 = t3;
+                }
+                t1 = t1.next;
+            }
+            t2 = t3 = null;
+        }
+    }
+    public void addToStart(CellPhone c)
+    {
+        head = new CellNode(c,head);
+    }
+    public void insertAtIndex(CellPhone c, int i) //throws NoSuchElementException
+    {
+
+    }
 }
